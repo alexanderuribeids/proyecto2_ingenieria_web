135ced1d8e05880aa33b7285d5ed1cfe6d2c617a
alexanderuribeids <alexanderuribeids@gmail.com>
2022-08-08 22:07:22 -0500

    Cambio vers 5. actualización css

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -3,11 +3,9 @@ import { Outlet } from 'react-router-dom';
 const Layout = () => {
   return (
     <main className='h-screen flex flex-col'>
-      <nav className='bg-gray-500 p-4'>NavBar</nav>
       <div className='h-full'>
         <Outlet />
       </div>
-      <footer className='bg-gray-300 p-3'>Footer</footer>
     </main>
   );
 };
--- src/pages/iphonex.jsx
+++ src/pages/iphonex.jsx
@@ -3,13 +3,18 @@ import { LinkComponent } from 'components/LinkComponent';
 const Iphonex = () => (
   <main>
     <div>
-      <h1>Hoja de vida para iphonex</h1>
+      <h1>Alexander Uribe González</h1>
+      <div box>
+        <p>
+          +573006942501 alexanderuribeids@gmail.com
+          https://github.com/alexanderuribeids?tab=repositories
+          www.linkedin.com/in/alexander-uribe-gonzalez-2a0836131 Street 42 59 BB
+          35 Tower 3 Apto. 713 Bello - Colombia
+        </p>
+      </div>
       <h2>
         <LinkComponent path='/' text='Ir a página principal' />
       </h2>
-    </div>
-    <div>
-      <img src='/img/iphonex.png' alt='Iphonex' />
     </div>
   </main>
 );
